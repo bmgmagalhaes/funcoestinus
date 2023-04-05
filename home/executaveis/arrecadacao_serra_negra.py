3281f74e4262ff515b19be480cd63023a538b668
bmgmagalhaes <bm.goncalves@gmail.com>
2023-04-05 15:46:36 -0300

    Adicionando GitIgnore e Novo Õrgão em SNN

--- home/executaveis/arrecadacao_serra_negra.py
+++ home/executaveis/arrecadacao_serra_negra.py
@@ -1,6 +1,7 @@
 import os
-from datetime import datetime
+# from datetime import datetime
 from arrecadacao_descompactar import verificar_arquivo_zip, descompactar_arquivo
+from utilitarios import pegar_data_pagamento_arquivo_retorno
 # from juncao_simples import executar_simples
 
 def executar_serra_negra(pasta_municipio):
@@ -22,29 +23,25 @@ def executar_serra_negra(pasta_municipio):
         try:
             caminho_completo = os.path.join(pasta_municipio, arquivo)
             with open(caminho_completo, 'r+') as retorno:
+                
                 header = retorno.readline()
                 detalhe = retorno.readlines()
-                tamanho = len(header)
-
-                if tamanho == 241:
-                    data_ficha = datetime.strptime(header[143:151], '%d%m%Y')
-                    data_ficha = datetime.strftime(data_ficha, '%y%m%d')
-                elif tamanho == 401:
-                    data_cobranca = datetime.strptime(header[94:100],'%d%m%y')
-                    data_cobranca = datetime.strftime(data_cobranca, '%y%m%d')
-                elif tamanho == 151:
-                    data_convenio = detalhe[0][23:29]
-
+                
+                if not 'DAF607' in arquivo:
+                    data = pegar_data_pagamento_arquivo_retorno(
+                        header, detalhe)
+                
         except Exception as e:
             # Quando arquivo do tesouro for alterado, esse teste ignora parte da lista inconsistente
             print('ERRO',e)
             continue
-
+        
         # if 'DAF607' in arquivo:
         #     executar_simples(pasta_municipio)
 
 
         try:
+            nome_arquivo = rf'{pasta_municipio}\MR{data}'
             if 'MUNICIPIO DE SERRA NEGRA DO NOBANCO DO BRASIL' in header:
                 # TESTAR T e U - VERIFICAR SE TEM PAGAMENTO
                 for linha_pagamento in detalhe:
@@ -52,22 +49,23 @@ def executar_serra_negra(pasta_municipio):
                         registro_de_pagamento = True
                         break
                 if registro_de_pagamento:
-                    os.rename(caminho_completo, rf'{pasta_municipio}\MR{data_ficha}.001')
+                    os.rename(caminho_completo, rf'{nome_arquivo}.001')
                 else:
                     # REMOVE SE NÃO HOUVER PAGAMENTO
                     os.remove(caminho_completo)
                 registro_de_pagamento = False
-            elif '272639000000MUNICIPIO DE SERRA NEGRA DO NO001BANCO DO BRASIL' in header:
-                os.rename(caminho_completo, rf'{pasta_municipio}\MR{data_cobranca}.002')
 
-            # elif '237493000000MUNICIPIO DE SERRA NEGRA DO NO001BANCO DO BRASIL' in header:
-            #     os.rename(caminho_completo, rf'{pasta_municipio}\MR{data_cobranca}.003')
+            elif '272639000000MUNICIPIO DE SERRA NEGRA DO NO001BANCO DO BRASIL' in header:
+                os.rename(caminho_completo, rf'{nome_arquivo}.002')
+
+            elif 'PM S NEGRA DO NORTE 104CAIXA' in header:
+                os.rename(caminho_completo, rf'{nome_arquivo}.104')
 
             elif '570168000000MUNICIPIO DE SERRA NEGRA DO NO001BANCO DO BRASIL' in header:
-                os.rename(caminho_completo, rf'{pasta_municipio}\MR{data_cobranca}.004')
+                os.rename(caminho_completo, rf'{nome_arquivo}.004')
 
             elif 'SERRA NEGRA NORTE TR001BANCO DO BRASIL  S/A' in header:
-                os.rename(caminho_completo, rf'{pasta_municipio}\MR{data_convenio}.005')
+                os.rename(caminho_completo, rf'{nome_arquivo}.005')
 
             else:
                 # REMOVE SE NÃO HOUVER PAGAMENTO
